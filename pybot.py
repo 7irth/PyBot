8e34bdcf56b1ebc663f7f4b1a7d8ce2343b8473c
Tirth <tirthp19@gmail.com>
2015-03-23 02:40:29 -0400

    It's alive!

--- pybot.py
+++ pybot.py
@@ -3,6 +3,8 @@ __author__ = 'Tirth Patel <complaints@tirthpatel.com>'
 from PIL import ImageGrab
 import os
 import time
+import win32api as windows
+import win32con
 
 
 def screen_grab(x=0, y=0, x_size=1920, y_size=1080):
@@ -11,5 +13,15 @@ def screen_grab(x=0, y=0, x_size=1920, y_size=1080):
                str(int(time.time())) + '.png', 'PNG')
 
 
+def scroll_up():
+    windows.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, 100)
+
+
+def left_click():
+    windows.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.1)
+    windows.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+
 if __name__ == '__main__':
-    screen_grab()
+    left_click()
