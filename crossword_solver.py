cf6a10b35b13e1c1dae492b041a0d50ffb760013
Tirth <tirthp19@gmail.com>
2015-04-21 02:38:33 -0400

    Substantial increase in speed and accuracy, no more overlaps

--- crossword_solver.py
+++ crossword_solver.py
@@ -4,13 +4,15 @@ from requests import get
 from re import findall
 from time import sleep
 
+delimiter = ' '
+
 
 class Clue:
     def __init__(self, number, coords, orientation, clue, length, answer=None):
         self.number = number
         self.orientation = orientation
         self.row, self.col = coords[0], coords[1]
-        self.clue = clue
+        self.clue = clue + ' ({0})'.format(str(length))
         self.answers = [answer] if answer else None
         self.length = length
 
@@ -23,7 +25,7 @@ class Clue:
 class Crossword:
     def __init__(self, size=13):
         self.size = size
-        self.puzzle = [['*' for _ in range(size)] for _ in range(size)]
+        self.puzzle = [[delimiter for _ in range(size)] for _ in range(size)]
         self.across, self.down = {}, {}
 
         # sample, Guardian #14,022
@@ -85,82 +87,101 @@ class Crossword:
         self.down[17] = Clue(17, (9, 9), 'down', 'Seethe', 4)
 
         self.test_ans = {
-            'Colour between red and green?': [(4, 'amber'), (2, 'green'),
-                                              (2, 'omani')],
-            'Beef, venison or lamb, for example': [(4, 'redmeat'),
-                                                   (2, 'navarin'),
-                                                   (2, 'persian'),
-                                                   (1, 'marsala'),
-                                                   (1, 'gamiest'),
-                                                   (1, 'reddeer')],
-            'Sharp tug': [(5, 'yank'), (2, 'jerk'), (1, 'boat'), (1, 'atwo'),
-                          (1, 'noon'), (1, 'edge')],
-            'Contest with the outcome in doubt right to the end': [
-                (4, 'cliffhanger'), (3, 'bloodvessel')],
-            'Herb used in cooking - a Mr Major (anag)': [(4, 'marjoram'),
-                                                         (3, 'rosemary'),
-                                                         (3, 'snapbean'),
-                                                         (2, 'beefcake')],
-            'Sandglass that runs for three (or four?) minutes': [
-                (4, 'eggtimer'),
-                (2, 'csimiami'),
-                (2, 'goeslong')],
-            'European country': [(5, 'austria'), (5, 'andorra'),
-                                 (2, 'finland'),
-                                 (2, 'albania'), (2, 'ukraine'),
-                                 (2, 'germany'),
-                                 (2, 'denmark'), (2, 'turkish'),
-                                 (2, 'estonia')],
-            'Brusque': [(5, 'rushing'), (5, 'raucous'), (5, 'laconic'),
-                        (5, 'offhand')],
-            'Transmission in a motor vehicle': [(4, 'gearbox'), (2, 'renault'),
-                                                (2, 'peugeot')],
-            'Ballroom dance in triple time': [(4, 'waltz'), (3, 'samba'),
-                                              (3, 'valse'), (3, 'rumba'),
-                                              (2, 'bossa'), (2, 'polka')],
-            'Cocktail crustacean?': [(2, 'prawn'), (1, 'palps'), (1, 'eliot')],
-            'Something sensational, daring or erotic': [(4, 'hotstuff'),
-                                                        (1, 'ferocity')],
-            'Hard, black wood': [(3, 'ebony'), (2, 'emery'), (1, 'hoeft'),
-                                 (1, 'maori')],
-            'Regardless': [(5, 'inadvertent'), (5, 'influential'),
-                           (5, 'indifferent'), (5, 'awestricken'),
-                           (5, 'inattentive'), (5, 'categorized'),
-                           (5, 'overlooking'), (5, 'interesting'),
-                           (5, 'opinionated'), (5, 'affectional'),
-                           (5, 'comewhatmay'), (5, 'appreciable'),
-                           (5, 'prestigious'), (5, 'consecrated'),
-                           (5, 'unconcerned'), (5, 'nonetheless'),
-                           (5, 'applaudable'), (5, 'considerate'),
-                           (5, 'approbative'), (5, 'insensitive'),
-                           (5, 'deferential'), (5, 'thoughtless'),
-                           (5, 'appreciated')],
-            'Massage to relieve tension by finger pressure': [
-                (4, 'reflexology'),
-                (2, 'rockthebaby'),
-                (2, 'breaktheice')],
-            'Amphibian (lodged in the throat?)': [(4, 'frog'), (3, 'rasp'),
-                                                  (2, 'ahem'), (1, 'fell')],
-            'Seethe': [(5, 'fill'), (5, 'flip'), (5, 'stew'), (5, 'boil'),
-                       (5, 'warm'), (5, 'cook'), (5, 'buzz'), (5, 'rage'),
-                       (5, 'burn'), (5, 'fume'), (5, 'brew'), (5, 'fizz'),
-                       (5, 'hiss'), (5, 'foam'), (5, 'soak'), (5, 'heat')],
-            'Olympic field event in which Greg Rutherford won gold in 2012': [
-                (4, 'longjump'), (3, 'roadtrip'), (2, 'sarajevo')],
-            'Fishing harbour in northeast Scotland': [(4, 'wick'), (2, 'said'),
-                                                      (2, 'tema'), (2, 'hull'),
-                                                      (1, 'suez'),
-                                                      (1, 'iasi')],
-            'Loire or California grape - NBC Channel I (anag)': [
-                (4, 'cheninblanc')]}
+            'Herb used in cooking - a Mr Major (anag) (8)': [(4, 'marjoram'),
+                                                             (3, 'rosemary'),
+                                                             (3, 'snapbean'),
+                                                             (2, 'beefcake')],
+            'Colour between red and green? (5)': [(5, 'amber'), (2, 'green'),
+                                                  (2, 'omani')],
+            'Sharp tug (4)': [(5, 'jerk'), (4, 'yank'), (1, 'boat')],
+            'European country (7)': [(5, 'ukraine'), (5, 'albania'),
+                                     (5, 'finland'), (4, 'austria'),
+                                     (4, 'andorra'), (2, 'germany'),
+                                     (2, 'denmark'), (2, 'turkish'),
+                                     (2, 'estonia'), (2, 'romania'),
+                                     (2, 'georgia'), (2, 'croatia')],
+            'Amphibian (lodged in the throat?) (4)': [(5, 'frog'), (3, 'rasp'),
+                                                      (2, 'ahem'),
+                                                      (1, 'fell')],
+            'Ballroom dance in triple time (5)': [(5, 'waltz'), (3, 'samba'),
+                                                  (3, 'valse'), (3, 'rumba'),
+                                                  (2, 'bossa'), (2, 'polka')],
+            'Cocktail crustacean? (5)': [(5, 'prawn'), (1, 'palps'),
+                                         (1, 'eliot'),
+                                         (1, 'crust'), (1, 'cosmo')],
+            'Sandglass that runs for three (or four?) minutes (8)': [
+                (4, 'eggtimer'), (2, 'csimiami'), (2, 'goeslong')],
+            'Fishing harbour in northeast Scotland (4)': [(5, 'wick'),
+                                                          (2, 'said'),
+                                                          (2, 'tema'),
+                                                          (2, 'hull'),
+                                                          (1, 'suez'),
+                                                          (1, 'iasi')],
+            'Transmission in a motor vehicle (7)': [(5, 'gearbox'),
+                                                    (2, 'renault'),
+                                                    (2, 'peugeot')],
+            'Loire or California grape - NBC Channel I (anag) (11)': [
+                (4, 'cheninblanc')],
+            'Regardless (11)': [(4, 'approbative'), (4, 'deferential'),
+                                (4, 'appreciated'), (4, 'influential'),
+                                (4, 'categorized'), (4, 'opinionated'),
+                                (4, 'comewhatmay'), (4, 'appreciable'),
+                                (4, 'prestigious'), (4, 'applaudable'),
+                                (4, 'insensitive'), (4, 'thoughtless'),
+                                (4, 'approbatory'), (4, 'calculating'),
+                                (4, 'awestricken'), (4, 'inadvertent'),
+                                (4, 'indifferent'), (4, 'inattentive'),
+                                (4, 'overlooking'), (4, 'interesting'),
+                                (4, 'affectional'), (4, 'consecrated'),
+                                (4, 'unconcerned'), (4, 'nonetheless'),
+                                (4, 'considerate')],
+            'Beef, venison or lamb, for example (7)': [(4, 'redmeat'),
+                                                       (2, 'navarin'),
+                                                       (2, 'persian'),
+                                                       (1, 'marsala'),
+                                                       (1, 'gamiest'),
+                                                       (1, 'reddeer')],
+            'Hard, black wood (5)': [(5, 'ebony'), (2, 'emery'), (1, 'hoeft'),
+                                     (1, 'maori')],
+            'Something sensational, daring or erotic (8)': [(4, 'hotstuff'),
+                                                            (1, 'ferocity')],
+            'Massage to relieve tension by finger pressure (11)': [
+                (5, 'reflexology'), (2, 'rockthebaby'), (2, 'breaktheice')],
+            'Olympic field event in which Greg Rutherford won gold in 2012 ('
+            '8)': [
+                (4, 'longjump'), (3, 'roadtrip'), (2, 'sarajevo'),
+                (2, 'grenoble')],
+            'Brusque (7)': [(5, 'offhand'), (4, 'laconic'), (4, 'rushing'),
+                            (4, 'raucous')],
+            'Contest with the outcome in doubt right to the end (11)': [
+                (5, 'cliffhanger'), (3, 'bloodvessel'), (1, 'goldenbrown')],
+            'Seethe (4)': [(5, 'fume'), (4, 'foam'), (4, 'soak'), (4, 'heat'),
+                           (4, 'boil'), (4, 'warm'), (4, 'buzz'), (4, 'rage'),
+                           (4, 'fizz'), (4, 'fill'), (4, 'flip'), (4, 'stew'),
+                           (4, 'cook'), (4, 'burn'), (4, 'brew'), (4, 'hiss')]}
 
-        self.clues = ([clue for clue in self.across.values()] +
-                      [clue for clue in self.down.values()])
+        self.clues = []
+        across_keys = sorted(self.across.keys())
+        down_keys = sorted(self.down.keys())
+
+        a_at, d_at = 0, 0
+        for i in range((max(len(self.across), len(self.down)))):
+            if a_at < len(self.across):
+                self.clues.append(self.across[across_keys[a_at]])
+                a_at += 1
+
+            if d_at < len(self.down):
+                self.clues.append(self.down[down_keys[d_at]])
+                d_at += 1
 
         self.fill_answers()
 
     def fill_answers(self):
+        counter = 0
         for clue in self.clues:
+            counter += 1
+            # print(str(counter * 100 // len(self.clues)) + '%')
+
             # clue.answers = get_answers(clue)
             clue.answers = self.test_ans[clue.clue]
 
@@ -168,31 +189,58 @@ class Crossword:
                 print(clue, 'no answers found :(')
                 continue
 
+            collision = False
+
             for answer in clue.answers:
-                for i in range(clue.length):
 
-                    if clue.orientation == 'across':
-                        curr = self.puzzle[clue.row][clue.col + i]
-                        if curr == '*':
-                            self.puzzle[clue.row][clue.col + i] = answer[1][i]
-                            # elif curr != answer[i]:
-                            #     print('mismatch!', answer)
-                            #     break
+                if clue.orientation == 'across':
 
-                    elif clue.orientation == 'down':
-                        curr = self.puzzle[clue.row + i][clue.col]
-                        if curr == '*':
-                            self.puzzle[clue.row + i][clue.col] = answer[1][i]
-                            # elif curr != answer[i]:
-                            #     print('mismatch!', answer)
-                            #     break
+                    current_word = ''
+                    for i in range(clue.length):
+                        current_word += self.puzzle[clue.row][clue.col + i]
+                        if (current_word[i] != delimiter and
+                                    answer[1][i] != current_word[i]):
+                            print("whoop", answer[1][i])
+                            collision = True
+                            break
+
+                    if collision:
+                        collision = False
+                        continue
+
+                    print(current_word)
+
+                    for i in range(clue.length):
+                        self.puzzle[clue.row][clue.col + i] = answer[1][i]
+                    break
+
+                elif clue.orientation == 'down':
+
+                    current_word = ''
+                    for i in range(clue.length):
+                        current_word += self.puzzle[clue.row + i][clue.col]
+                        if (current_word[i] != delimiter and
+                                    answer[1][i] != current_word[i]):
+                            print("whoop", answer[1][i])
+                            collision = True
+                            break
+
+                    if collision:
+                        collision = False
+                        continue
+
+                    print(current_word)
+
+                    for i in range(clue.length):
+                        self.puzzle[clue.row + i][clue.col] = answer[1][i]
+                    break
 
     def __str__(self):
         s = ''
         for i in range(self.size):
             for j in range(self.size):
-                s += str(self.puzzle[i][j])
-            s += '\n'
+                s += str(self.puzzle[i][j]) + '  '
+            s += '\n'  # + '-' * self.size * 2 + '\n'
 
         return s
 
