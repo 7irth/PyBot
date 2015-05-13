62efc6b48480d00785a3994f2dd3d4d1d4b0266a
Tirth Patel <complaints@tirthpatel.com>
2015-05-12 22:22:49 -0400

    Extract SMS from TitaniumBackups

--- messages/sms.py
+++ messages/sms.py
@@ -1,7 +1,10 @@
 __author__ = 'Tirth Patel <complaints@tirthpatel.com>'
 
 import sqlite3
+from datetime import datetime
+from os import makedirs
 from bs4 import BeautifulSoup
+import phonenumbers
 from messages.stuff import *
 
 
@@ -44,16 +47,46 @@ def get_db_entry(database, ts):
     return c.execute('SELECT * FROM sms WHERE date_sent=?', (ts,)).fetchall()
 
 
-def extract_messages_titanium(file):
+def extract_messages_titanium(file, out_folder='t_sms'):
+    makedirs(out_folder, exist_ok=True)
+
     with open(file + '.xml', 'rb') as sms:
-        soup = BeautifulSoup(sms)
-        print(soup.thread)
+        for thread in BeautifulSoup(sms)('thread'):
+            convo = thread['address']
+
+            try:
+                number = str(phonenumbers.parse(convo, "CA").national_number)
+            except phonenumbers.NumberParseException:
+                # print([phonenumbers.parse(n, "CA").national_number for n in convo.split(';')])
+                continue  # group message
+
+            with open(out_folder + '/' + number + '.txt', 'w') as current:
+                for msg in thread.find_all('sms'):
+                    try:
+                        date = msg['datesent']
+                    except KeyError:
+                        date = msg['date']
+
+                    sent = '{0:0<13}'.format(str(datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ').timestamp()).replace('.', ''))
+                    sender = 'me' if msg['msgbox'] == 'sent' else number
+                    body = msg.string
+
+                    if len(sent) < 2:
+                        print(msg.attrs)
+                        exit()
+
+                    try:
+                        current.write(sent + SEPR + sender + SEPR + body + '\n')
+                    except UnicodeEncodeError:
+                        print(str(msg.string).encode())
+
+                current.close()
 
 
 def go():
-    extract_messages_sqlite('mmssms.db')
-
+    # extract_messages_sqlite('mmssms.db')
+    extract_messages_titanium('sms')
 
 if __name__ == '__main__':
     go()
-    # conversation_frequency('sms_all', graph=True)
+    conversation_frequency('', graph=True)
